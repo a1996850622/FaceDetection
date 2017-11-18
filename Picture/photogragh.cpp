7d1a33b242ed6b4467a1f2c3e90b5bfc649b7472
a1996850622 <a1996850622@gmail.com>
2017-11-19 02:00:36 +0800

    Update

--- Picture/photogragh.cpp
+++ Picture/photogragh.cpp
@@ -9,22 +9,10 @@ using namespace cv;
 void PANIC(char *msg);
 #define PANIC(msg){perror(msg); exit(-1);}
 
-/** Gloabal variables **/
-String face_cascade_name = "../Source/haarcascade_frontalface_default.xml";
-String eyes_cascade_name = "../Source/haarcascade_eye_tree_eyeglasses.xml";
-CascadeClassifier face_cascade; // Define the face classifier
-CascadeClassifier eyes_cascade; // Define the eyes classifier
-
 int main(void){
 	VideoCapture capture = VideoCapture(0);
-	std::vector<Rect> faces;
 	Mat frame, frame_gray;
 	int i = 1;
-
-	if (!face_cascade.load(face_cascade_name)) 
-		PANIC("Error loading face cascade");
-	if (!eyes_cascade.load(eyes_cascade_name))
-		PANIC("Error loading eyes cascade");
 
 	cout << "Press p to take a picture" << endl;
 	while(1){
@@ -34,7 +22,7 @@ int main(void){
 		if(frame.empty())
             PANIC("Error capture frame");
 
-		imshow("photo", frame);
+        imshow("frame", frame);
 		String filename = format("picture%d.jpg", i);
 
 		/** Press p to take pictures **/
